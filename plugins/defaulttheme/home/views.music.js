0a9c93ebad17da4cfd6ae8a38f044e5ae7c133df
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-08-03 20:04:00 -0400

    update music

--- plugins/defaulttheme/home/views.music.js
+++ plugins/defaulttheme/home/views.music.js
@@ -4,7 +4,8 @@
 
         var options = {
 
-            Limit: 24,
+            IncludeItemTypes: "Audio",
+            Limit: 9,
             Fields: "PrimaryImageAspectRatio",
             ParentId: parentId,
             ImageTypeLimit: 1,
@@ -13,11 +14,96 @@
 
         apiClient.getJSON(apiClient.getUrl('Users/' + apiClient.getCurrentUserId() + '/Items/Latest', options)).done(function (result) {
 
-            var resumeSection = element.querySelector('.latestSection');
+            var section = element.querySelector('.latestSection');
 
             DefaultTheme.CardBuilder.buildCards(result, apiClient, {
-                parentContainer: resumeSection,
-                itemsContainer: resumeSection.querySelector('.itemsContainer'),
+                parentContainer: section,
+                itemsContainer: section.querySelector('.itemsContainer'),
+                shape: 'autoHome',
+                width: DefaultTheme.CardBuilder.homePortraitWidth
+            });
+        });
+    }
+
+    function loadPlaylists(element, parentId, apiClient) {
+
+        var options = {
+
+            SortBy: "SortName",
+            SortOrder: "Ascending",
+            IncludeItemTypes: "Playlist",
+            Recursive: true,
+            ParentId: parentId,
+            Fields: "PrimaryImageAspectRatio,SortName,CumulativeRunTimeTicks,CanDelete",
+            StartIndex: 0,
+            Limit: 9
+        };
+
+        apiClient.getItems(apiClient.getCurrentUserId(), options).done(function (result) {
+
+            var section = element.querySelector('.playlistsSection');
+
+            DefaultTheme.CardBuilder.buildCards(result.Items, apiClient, {
+                parentContainer: section,
+                itemsContainer: section.querySelector('.itemsContainer'),
+                shape: 'autoHome',
+                width: DefaultTheme.CardBuilder.homePortraitWidth
+            });
+        });
+    }
+
+    function loadRecentlyPlayed(element, parentId, apiClient) {
+
+        var options = {
+
+            SortBy: "DatePlayed",
+            SortOrder: "Descending",
+            IncludeItemTypes: "Audio",
+            Limit: 9,
+            Recursive: true,
+            Fields: "PrimaryImageAspectRatio",
+            Filters: "IsPlayed",
+            ParentId: parentId,
+            ImageTypeLimit: 1,
+            EnableImageTypes: "Primary,Backdrop,Thumb"
+        };
+
+        apiClient.getItems(apiClient.getCurrentUserId(), options).done(function (result) {
+
+            var section = element.querySelector('.recentlyPlayedSection');
+
+            DefaultTheme.CardBuilder.buildCards(result.Items, apiClient, {
+                parentContainer: section,
+                itemsContainer: section.querySelector('.itemsContainer'),
+                shape: 'autoHome',
+                width: DefaultTheme.CardBuilder.homePortraitWidth
+            });
+        });
+    }
+
+    function loadFrequentlyPlayed(element, parentId, apiClient) {
+
+        var options = {
+
+            SortBy: "PlayCount",
+            SortOrder: "Descending",
+            IncludeItemTypes: "Audio",
+            Limit: 9,
+            Recursive: true,
+            Fields: "PrimaryImageAspectRatio",
+            Filters: "IsPlayed",
+            ParentId: parentId,
+            ImageTypeLimit: 1,
+            EnableImageTypes: "Primary,Backdrop,Thumb"
+        };
+
+        apiClient.getItems(apiClient.getCurrentUserId(), options).done(function (result) {
+
+            var section = element.querySelector('.frequentlyPlayedSection');
+
+            DefaultTheme.CardBuilder.buildCards(result.Items, apiClient, {
+                parentContainer: section,
+                itemsContainer: section.querySelector('.itemsContainer'),
                 shape: 'autoHome',
                 width: DefaultTheme.CardBuilder.homePortraitWidth
             });
@@ -32,6 +118,9 @@
             var apiClient = connectionManager.currentApiClient();
 
             loadLatest(element, parentId, apiClient);
+            loadPlaylists(element, parentId, apiClient);
+            loadRecentlyPlayed(element, parentId, apiClient);
+            loadFrequentlyPlayed(element, parentId, apiClient);
         });
 
         self.destroy = function () {
