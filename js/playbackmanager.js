aeaa3dbba088b02889bdbeee21125739714fd1ed
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-08-12 14:28:43 -0400

    add logo screensaver

--- js/playbackmanager.js
+++ js/playbackmanager.js
@@ -1,15 +1,63 @@
-(function (globalScope, document) {
+(function (globalScope) {
 
-	function isPlayingVideo() {
-		return false;
-	}
+    function playbackManager() {
+
+        var self = this;
+        self.players = [];
+
+        self.isPlayingVideo = function () {
+            return false;
+        };
+
+        self.canPlay = function (item) {
+
+        };
+
+        self.isMuted = function () {
+
+        };
+
+        self.setMute = function (mute) {
+
+        };
+
+        self.getVolume = function () {
+
+        };
+
+        self.setVolume = function (volume) {
+
+        };
+
+        self.volumeUp = function () {
+
+        };
+
+        self.volumeDown = function () {
+
+        };
+
+        self.setAudioStreamIndex = function (index) {
+
+        };
+
+        self.setSubtitleStreamIndex = function (index) {
+
+        };
+
+        self.stopAll = function () {
+
+        };
+
+        self.getPlayers = function () {
+            return players;
+        };
+    }
 
     if (!globalScope.Emby) {
         globalScope.Emby = {};
     }
 
-    globalScope.Emby.PlaybackManager = {
-        isPlayingVideo: isPlayingVideo
-    };
+    globalScope.Emby.PlaybackManager = new playbackManager();
 
-})(this, document);
+})(this);
